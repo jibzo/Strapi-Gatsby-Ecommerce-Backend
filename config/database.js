180f7e0790e375340a1d4a98d3c463c3ab454108
Jibran Javed <jibran@nakedcreativity.co.uk>
2020-10-25 19:01:18 +0000

    updated database

--- config/database.js
+++ config/database.js
@@ -1,47 +1,15 @@
-const parse = require('pg-connection-string').parse;
-
-if(process.env.DATABASE_URL != undefined) {
-
-  const config = parse(process.env.DATABASE_URL)
-} 
-
-module.exports = ({ env }) => {
-  
-  if(env('NODE_ENV') === 'production'){
-    return {
-      defaultConnection: 'default',
-      connections: {
-        default: {
-          connector: 'bookshelf',
-          settings: {
-            client: 'postgres',
-            host: config.host,
-            port: config.port,
-            database: config.database,
-            username: config.user,
-            password: config.password,
-          },
-          options: {
-            ssl: false,
-          },
-        },
+module.exports = ({ env }) => ({
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'bookshelf',
+      settings: {
+        client: 'sqlite',
+        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
       },
-    }
-  } else {
-    return {
-      defaultConnection: 'default',
-      connections: {
-        default: {
-          connector: 'bookshelf',
-          settings: {
-            client: 'sqlite',
-            filename: env('DATABASE_FILENAME', '.tmp/data.db'),
-          },
-          options: {
-            useNullAsDefault: true,
-          },
-        },
+      options: {
+        useNullAsDefault: true,
       },
-    }
-  }
-};
+    },
+  },
+});
